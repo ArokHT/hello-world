e96791f6de6b89b57a0cb917e5531158b6492809
ArokHT <37290596+ArokHT@users.noreply.github.com>
2018-07-04 00:27:59 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,4 +1,14 @@
 #include "HelloWorld.h"
+#include<iostream>
+using namespace std;
+class HelloWorld
+{
+  public:
+    void hello()
+    {
+      cout << "Hello World" << endl;
+    }
+}
 int main() {
   HelloWorld hw;
   hw.hello();
