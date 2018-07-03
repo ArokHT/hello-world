f4ee0d45ebf36e9273c43b5ba312f8c5ee1edfae
ArokHT <37290596+ArokHT@users.noreply.github.com>
2018-07-03 23:46:58 +0800

    Update Date.cpp

--- Date.cpp
+++ Date.cpp
@@ -1,10 +1,12 @@
+#include<iostream>
+using namespace std;
 class Date {
 public:
   Date(int y, int m, int d);
   ~Date();
 
-  void nextDay();
-  void previousDay();
+  void nextDay(int y, int m, int d);
+  void previousDay(int y, int m, int d);
   bool equal(Date other);
   void show();
 private:
@@ -12,7 +14,24 @@ private:
   int month;
   int day;
 };
-
+Date::Date(int y, int m, int d)
+{
+  year=y;
+  month=m;
+  day=d;
+}
+void Date::nextDay(int y, int m, int d)
+{
+  year=y;
+  month=m;
+  day=d+1;
+}
+void Date::previousDay(int y, int m, int d)
+{
+  year=y;
+  month=m;
+  day=d-1;
+}
 int main() {
   Date today(2018,3,20);
   Date day(2018,3,20);
