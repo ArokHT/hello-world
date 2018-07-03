78c76026baff2b9d86f2750c026f5f74602b5157
ArokHT <37290596+ArokHT@users.noreply.github.com>
2018-07-04 00:40:34 +0800

    Update Parallelogram.cpp

--- Parallelogram.cpp
+++ Parallelogram.cpp
@@ -1,10 +1,41 @@
-class Parallelogram {
+#include<iostream>
+using namespace std;
+class Parallelogram
+{
+  public:
+  	Parallelogram(int x, int y);
+    void show();
+    void area()
+    {
+      cout << height1*length1 << endl;
+    }
+    void height(int y)
+    {
+      height1=y;
+    }
+    void length(int x)
+    {
+      length1=x;
+    }
+    private:
+    	int height1;
+    	int length1;
 };
-int main() {
-  Parallelogram pg;
+Parallelogram::Parallelogram(int x, int y)
+{
+	height1=y;
+	length1=x;
+}
+void Parallelogram::show()
+{
+  Parallelogram::area();
+}
+int main()
+{
+  Parallelogram pg(1, 1);
   pg.show();
   pg.area();
-  pg.height();
-  pg.length();
+  pg.height(1);
+  pg.length(1);
   return 0;
 }
