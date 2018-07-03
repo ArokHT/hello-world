c86ed9b7e12823c4f76a1750d1fc165cc8e26810
ArokHT <37290596+ArokHT@users.noreply.github.com>
2018-07-04 00:42:16 +0800

    Update ObjectLife.cpp

--- ObjectLife.cpp
+++ ObjectLife.cpp
@@ -1,4 +1,33 @@
-class {
+
+#include <iostream>
+ 
+using namespace std;
+ 
+class Test
+{
+public:
+    int x_;
+    void init(int x, int y,int z);
+    void display()
+    {
+        cout<< x_ << endl;
+    };
+private:
+    int y_;
+protected:
+    int z_;
 };
-int main() {
+ 
+void Test::init(int x, int y, int z)
+{
+    x_ = x;
+    y_ = y;
+    z_ = z;
+}
+int main()
+{
+    Test t;
+    t.init(1,2,3);
+    t.display();
+    return 0;
 }
