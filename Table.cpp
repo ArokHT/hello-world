f6634dd70a3425e6c522b03b44a249d11ac7c6b1
ArokHT <37290596+ArokHT@users.noreply.github.com>
2018-07-04 00:23:55 +0800

    Update Table.cpp

--- Table.cpp
+++ Table.cpp
@@ -1,3 +1,56 @@
+#include <iostream>  
+#include <string.h>  
+using namespace std;
+int _tmain(int argc, _TCHAR* argv[])
+{
+    lua_State *L = luaL_newstate();  
+ 
+	luaL_openlibs(L);
+	luaL_dofile(L,"Hello.lua");
+	lua_getglobal(L,"str");
+	lua_getfield(L,1,"name");
+	lua_getfield(L,1,"age");
+ 
+	if(lua_isstring(L,2))
+	{
+		cout<<"name= "<<lua_tostring(L,-2)<<endl;
+	}
+	if(lua_isnumber(L,3))
+	{
+		cout<<"age= "<<lua_tonumber(L,-1)<<endl;
+	}
+	lua_pop(L,2);
+	lua_pushfstring(L,"So so");
+	lua_setfield(L,1,"name");
+	lua_getfield(L,1,"name");
+	if(lua_isstring(L,2))
+	{
+		cout<<"name1= "<<lua_tostring(L,2)<<endl;
+	}
+	lua_pop(L,2);
+	lua_newtable(L);
+	lua_pushnumber(L,11);
+	lua_pushstring(L,"New");
+	lua_setfield(L,1,"name");
+	lua_setfield(L,1,"age");
+	lua_getfield(L,1,"name");
+	lua_getfield(L,1,"age");
+	if(lua_isstring(L,2))
+	{
+		cout<<"name= "<<lua_tostring(L,2)<<endl;
+	}
+	if(lua_isnumber(L,3))
+	{
+		cout<<"age= "<<lua_tonumber(L,3)<<endl;
+	}
+ 
+	lua_pop(L,3);
+    lua_close(L);  
+	
+	int i;
+	cin>>i;
+    return 0 ;  
+}
 int main() {
   Table tb;
   tb.show();
