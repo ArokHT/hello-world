052d35665a06d5a864f8730436c056f9bc2fbc2e
ArokHT <37290596+ArokHT@users.noreply.github.com>
2018-07-04 00:19:00 +0800

    Update Prime.c

--- Prime.c
+++ Prime.c
@@ -1,64 +1,24 @@
 /*满足下列条件的自然数称为超级素数:该数本身,所有数字之和,所有数字之积以及所有数字的平方和都是素数.例如113就是一个超级素数.求[100,9999]之内:(1)超级素数的个数.(2)所有超级素数之和.(3)最大的超级素数.*/
-#include<iostream>
-#include<string>
-#include<cstring>
+#include <iostream>
 using namespace std;
-int main()
+int prime(int n);
+int prime(int n)
 {
-	int i, j, len1, len2, flag=0;
-	char s1[100], s2[100];
-	cin >> s1 >> s2;
-	len1=strlen(s1);
-	len2=strlen(s2);
-	if(s1[len1-1]==s2[len2-1] && len1<=len2)
-	{
-		for(i=len1-1, j=len2-1; i>=0; i--, j--)
-		{
-			if(s1[i]==s2[j])
-			{
-				flag++;
-			}
-			else
-			if(s1[i]!=s2[j]) 
-			{
-				break;
-			}
-		}
-		for(i=len1-flag; i<=len1-1; i++)
-		{
-			cout << s1[i];
-		}
-		cout << endl;
-	}
-	if(s1[len1-1]==s2[len2-1] && len1>len2)
-	{
-		for(i=len1-1, j=len2-1; j>=0; i--, j--)
-		{
-			if(s1[i]==s2[j])
-			{
-				flag++;
-			}
-			else
-			if(s1[i]!=s2[j])  
-			{
-				break;
-			}
-		}
-		for(j=len2-flag; j<=len2-1; j++)
-		{
-			cout << s2[j];
-		}
-		cout << endl;
-	}
-	if(s1[len1-1]!=s2[len2-1])
-	{
-		cout << "null" << endl;
-	}
-	//cout << s1[2] << endl;
-	//cout << len1 << endl;
-	//cout << len2 << endl;
-	//cout << flag << endl;
-	return 0;
+int i;
+for(i=2;i<=n/2;i++)
+if(n%i==0)
+return 0;
+else
+return 1;
+return 0;
+}
+void main(){
+int m;
+cout<<"请输入这个数m=";
+cin>>m;
+if(prime(m))
+cout << m << " 是素数 ";
+else cout << m << " 不是素数 ";
 }
 int sum_bit(int num) {
   return 0;
